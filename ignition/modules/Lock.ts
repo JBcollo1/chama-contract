235537501fe154fee07c5581c375f3f6be5f65c7
Collins <jbcollins254@gmail.com>
2025-07-18 10:29:23 +0300

    update ignition to ensure deployement of main contract reducing gas

--- ignition/modules/Lock.ts
+++ ignition/modules/Lock.ts
@@ -1,21 +1,11 @@
-// This setup uses Hardhat Ignition to manage smart contract deployments.
-// Learn more about it at https://hardhat.org/ignition
-
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-import { parseEther } from "viem";
 
-const JAN_1ST_2030 = 1893456000;
-const ONE_GWEI: bigint = parseEther("0.001");
+const ChamaFactoryModule = buildModule("ChamaFactoryModule", (m) => {
+  const owner = m.getParameter("owner", "0xYourWalletAddressHere");
 
-const LockModule = buildModule("LockModule", (m) => {
-  const unlockTime = m.getParameter("unlockTime", JAN_1ST_2030);
-  const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
+  const chamaFactory = m.contract("ChamaFactory", [owner]);
 
-  const lock = m.contract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  return { lock };
+  return { chamaFactory };
 });
 
-export default LockModule;
+export default ChamaFactoryModule;
