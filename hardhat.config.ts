e8db7c90ccb8d5c913e2d79621e3f4336aeaaf6d
Collins <jbcollins254@gmail.com>
2025-08-06 20:48:30 +0300

    deployment phase 1

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,8 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import * as dotenv from "dotenv";
 
+dotenv.config();
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.28",
@@ -10,6 +12,12 @@ const config: HardhatUserConfig = {
         runs: 200,
       },
       viaIR: true, 
+    },
+  },
+  networks: {
+    fuji: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     },
   },
 };
