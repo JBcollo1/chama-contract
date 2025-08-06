5d82388f25eb3097e015dea9d5df38728aaec5cc
Collins <jbcollins254@gmail.com>
2025-08-06 22:55:22 +0300

    final deployment

--- ignition/modules/Lock.ts
+++ ignition/modules/Lock.ts
@@ -1,7 +1,7 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const ChamaFactoryModule = buildModule("ChamaFactoryModule", (m) => {
-  const owner = m.getParameter("owner", "0xYourWalletAddressHere");
+  const owner = m.getParameter("owner", "0x5B058198Fc832E592edA2b749bc6e4380f4ED458");
 
   const chamaFactory = m.contract("ChamaFactory", [owner]);
 
